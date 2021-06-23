37eb34f8613613f2919becd7e84e5da7c3a883de
pppineappleOvO <496266706@qq.com>
2021-06-23 17:08:25 +0800

    000

--- project-mobile-丝芙兰/order-detail/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.header-back .logoImg').click(function(){
-        window.history.back();
-    })
-
-})
--- project-mobile-丝芙兰/personal-center/index.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function () {
-$('.guess-like a').attr('href','../product/index.html');
-
-})
