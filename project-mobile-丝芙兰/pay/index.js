4d21103ac73c0b34ce14abbf015b53cc0091ac75
pppineappleOvO <496266706@qq.com>
2021-06-23 17:02:36 +0800

    111

--- project-mobile-丝芙兰/pay/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('.header-back .logoImg').click(function(){
-        window.history.back();
-    })
-
-$('.address').click(function () {
-    window.location.href='../myAddress/index.html'
-})
-})
